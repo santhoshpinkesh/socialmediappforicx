5beff6368b5757c2ee409bf0d82a9e8e2af0fd64
santhoshpinkesh <158996813+santhoshpinkesh@users.noreply.github.com>
2024-02-07 15:19:30 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+  return render_template("home.html")
+
+
+@app.route("/test")
+def test():
+  return "test is working"
+
+
+@app.route("/history")
+def history():
+  return render_template("history.html")
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
